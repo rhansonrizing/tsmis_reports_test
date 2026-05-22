358a9c799e542706d09ca1a43b77ade51348c529
rhansonrizing <126808476+rhansonrizing@users.noreply.github.com>
2026-05-22 13:44:19 -0700

    Update configuration URLs for test environment

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
-  mapServiceUrl:    "https://rhapps-prod.dot.ca.gov/ars/rest/services/TSMIS/lrs_tsmis_prod/MapServer",
-  featureServiceUrl:"https://rhapps-prod.dot.ca.gov/ars/rest/services/TSMIS/lrs_tsmis_prod/FeatureServer",
-  vmsUrl:           "https://rhapps-prod.dot.ca.gov/ars/rest/services/TSMIS/lrs_tsmis_prod/VersionManagementServer",
-  oauthClientId:    "z7jnDQesKUG5xAxv",
-  oauthAuthorizeUrl:"https://rhapps-prod.dot.ca.gov/portal/sharing/rest/oauth2/authorize",
-  oauthTokenUrl:    "https://rhapps-prod.dot.ca.gov/portal/sharing/rest/oauth2/token",
-  oauthRedirectUrl: "https://rhansonrizing.github.io/tsmis_reports/index.html"
+  mapServiceUrl:    "https://rhapps-test.dot.ca.gov/ars/rest/services/TSNR/lrs_tsnr_test/MapServer",
+  featureServiceUrl:"https://rhapps-test.dot.ca.gov/ars/rest/services/TSNR/lrs_tsnr_test/FeatureServer",
+  vmsUrl:           "https://rhapps-test.dot.ca.gov/ars/rest/services/TSNR/lrs_tsnr_test/VersionManagementServer",
+  oauthClientId:    "tVmsJPOotsf9eRj2",
+  oauthAuthorizeUrl:"https://rhapps-test.dot.ca.gov/portal/sharing/rest/oauth2/authorize",
+  oauthTokenUrl:    "https://rhapps-test.dot.ca.gov/portal/sharing/rest/oauth2/token",
+  oauthRedirectUrl: "https://rhansonrizing.github.io/tsmis_reports_test/index.html"
 };
